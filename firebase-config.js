4a6c8c87119addf4dbefd29b99a9af3085490365
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-09-17 19:01:19 +0100

    third commit

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
-// firebase-config.js
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB...real_key_here",        // must be real
+  apiKey: "AIzaSyB...real_key_here",        
   authDomain: "your-project-id.firebaseapp.com",
   projectId: "your-project-id",
   storageBucket: "your-project-id.appspot.com",
@@ -8,7 +8,7 @@ const firebaseConfig = {
   appId: "1:1234567890:web:abc123def456"
 };
 
-// Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
